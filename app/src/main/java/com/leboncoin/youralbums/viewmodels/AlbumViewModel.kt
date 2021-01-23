de3a9c5ed84d381815e6161c54667b7b37795c7a
NGUYEN Huu Hien <hienka99@gmail.com>
2021-01-23 03:32:58 +0100

    Add ViewModel

---
+++ app/src/main/java/com/leboncoin/youralbums/viewmodels/AlbumViewModel.kt
@@ -0,0 +1,99 @@
+package com.leboncoin.youralbums.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.leboncoin.youralbums.database.getDatabase
+import com.leboncoin.youralbums.repository.AlbumsRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+/**
+ * @param application The application that this viewmodel is attached to, it's safe to hold a
+ */
+
+class AlbumViewModel(application: Application) : AndroidViewModel(application) {
+
+
+    /**
+     * The data source this ViewModel will fetch results from.
+     */
+    private val albumsRepository = AlbumsRepository(getDatabase(application))
+
+    /**
+     * Albums displayed on the screen.
+     */
+    val albums = albumsRepository.albums
+
+    /**
+     * Event triggered for network error. This is private to avoid exposing a
+     * way to set this value to observers.
+     */
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+
+    /**
+     * Event triggered for network error. Views should use this to get access
+     * to the data.
+     */
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+    /**
+     * Flag to display the error message. This is private to avoid exposing a
+     * way to set this value to observers.
+     */
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+
+    /**
+     * Flag to display the error message. Views should use this to get access
+     * to the data.
+     */
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+
+    /**
+     * init{} is called immediately when this ViewModel is created.
+     */
+    init {
+        refreshDataFromRepository()
+    }
+
+    /**
+     * Refresh data from the repository. Use a coroutine launch to run in a
+     * background thread.
+     */
+    private fun refreshDataFromRepository() {
+        viewModelScope.launch {
+            try {
+                albumsRepository.refreshAlbums()
+                _eventNetworkError.value = false
+                _isNetworkErrorShown.value = false
+
+            } catch (networkError: IOException) {
+                // Show a Toast error message and hide the progress bar.
+                if (albums.value.isNullOrEmpty())
+                    _eventNetworkError.value = true
+            }
+        }
+    }
+
+
+    /**
+     * Resets the network error flag.
+     */
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+    /**
+     * Factory for constructing AlbumViewModel with parameter
+     */
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(AlbumViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return AlbumViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+}
