1945232402f9bd4051b61ea7f29bd6322e44bbdd
NGUYEN Huu Hien <hienka99@gmail.com>
2021-01-23 12:06:04 +0100

    Add WorkManager to update automatically data

---
+++ app/src/main/java/com/leboncoin/youralbums/work/RefreshDataWorker.kt
@@ -0,0 +1,31 @@
+package com.leboncoin.youralbums.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.leboncoin.youralbums.database.getDatabase
+import com.leboncoin.youralbums.repository.AlbumsRepository
+import retrofit2.HttpException
+import timber.log.Timber
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "com.leboncoin.youralbums.work.RefreshDataWorker"
+    }
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = AlbumsRepository(database)
+
+        try {
+            repository.refreshAlbums()
+            Timber.d("WorkManager: Work request for sync is run")
+        } catch (e: HttpException) {
+            return Result.retry()
+        }
+
+        return Result.success()
+    }
+}
